7fcadf87c77517a440b4d805cbc8777ac7e5aaab
José Antonio <jatorrescoca@gmail.com>
2025-06-08 20:55:37 +0200

    test(features): Add authorization specs for Post management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "No tienes autorización para realizar esta acción."
+    redirect_back(fallback_location: root_path)
+  end
 end
